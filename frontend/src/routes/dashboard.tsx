6673fe49efc2faf0945514b1607659a30faca996
noircodes <ilhamizdhihar2007@gmail.com>
2025-04-17 16:02:42 +0700

    revamp fe

---
+++ frontend/src/routes/dashboard.tsx
@@ -0,0 +1,10 @@
+// import { LoaderFunctionArgs } from 'react-router';
+
+// export async function loader({ request }: LoaderFunctionArgs) {
+//   // simulate loading data
+//   return { stats: 'Example statistics data' };
+// }
+
+export default function Dashboard() {
+  return <div>Welcome to the Dashboard!</div>;
+}
